65e00490a3ac389d4867ea1d3a91dd1fbe4b840e
Alberto Dubois <aduboisr@gmail.com>
2014-06-19 16:12:48 +0200

    Second

--- ui.R
+++ ui.R
@@ -12,7 +12,12 @@ shinyUI(
     
   sliderInput("pl",'Change the value of the Petal Length',value=1,min=1,max=6.9,step=0.05),
   
-sliderInput("pw",'Change the value of the Petal Width',value=0.1,min=0.1,max=2.5,step=0.05)),
+sliderInput("pw",'Change the value of the Petal Width',value=0.1,min=0.1,max=2.5,step=0.05),
+
+  h3('Documentation:'),
+  h5('In this Shinny app we are going to predict the Species of the flower using a classification tree. The user only needs to change the sliders, which correspond to the charactheristics of our flower. There are 3 types of flowers: setosa, versicolor and virginica. Looking at the classification tree you will be able to see what variables yo need to change in order to alter the flower species and which variables dont affect at all! Good luck!')
+
+  ),
     
     mainPanel(
       h3('Prediction of the species:'),
